c0b969cdeb9408107bde5f5662e13cb21a8e0550
Jaroslav Imrich <jaroslav.imrich@gmail.com>
2013-12-31 13:07:27 +0100

    Initial project structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Pkcs11Interop.URI
 =================
+
+PKCS#11 URI extensions for Pkcs11Interop library
---
+++ src/Pkcs11Interop.URI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pkcs11Interop.URI")]
+[assembly: AssemblyDescription("PKCS#11 URI extensions for Pkcs11Interop library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JWC s.r.o.")]
+[assembly: AssemblyProduct("Pkcs11Interop.URI (AGPL)")]
+[assembly: AssemblyCopyright("Copyright © 2013-2014 JWC s.r.o. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("46ccb297-d34d-42a0-b8ca-f543de2f7301")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
